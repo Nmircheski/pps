9f2687544fdd57ff72f52d6fdc24b6e68bd6b8ae
Nikola Mircheski <nmircheski@gmail.com>
2023-12-26 15:29:40 +0100

    Add footer component.

--- pps/apps/pps/src/app/app.component.ts
+++ pps/apps/pps/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '@pps/ui/footer';
 import { NavbarComponent } from '@pps/ui/navbar';
 
 @Component({
   standalone: true,
-  imports: [NavbarComponent, RouterModule],
+  imports: [NavbarComponent, FooterComponent, RouterModule],
   selector: 'pps-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ pps/libs/ui/footer/src/lib/footer-nav/footer-nav.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterLink } from '../models/models';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'pps-footer-nav',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer-nav.component.html',
+  styleUrl: './footer-nav.component.scss',
+})
+export class FooterNavComponent {
+  @Input() title = '';
+
+  @Input() links: FooterLink[] = [];
+}
