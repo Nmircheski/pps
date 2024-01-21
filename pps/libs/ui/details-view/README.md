c54c04b9c50f3cc05901a4e8d0d579b746253d5c
Nikola Mircheski <nmircheski@gmail.com>
2024-01-21 16:28:48 +0100

    Add details view component

---
+++ pps/libs/ui/details-view/README.md
@@ -0,0 +1,7 @@
+# details-view
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test details-view` to execute the unit tests.
---
+++ pps/libs/ui/details-view/src/lib/details-view/details-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'pps-details-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './details-view.component.html',
+  styleUrl: './details-view.component.scss',
+})
+export class DetailsViewComponent {}
