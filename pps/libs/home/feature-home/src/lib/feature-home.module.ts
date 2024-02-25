51bce684cd9733e9d7f7c5e0d4ec7c194f521093
Nikola Mircheski <nmircheski@gmail.com>
2024-02-25 11:48:26 +0100

    Introduce image-details component

--- pps/libs/home/feature-home/src/lib/feature-home.module.ts
+++ pps/libs/home/feature-home/src/lib/feature-home.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BannerComponent } from '@pps/ui/banner';
 import { CardComponent } from '@pps/ui/card';
+import { DetailsViewComponent } from '@pps/ui/details-view';
 import { ExpandablePanelComponent } from '@pps/ui/expandable-panel';
 import { HomepageComponent } from './homepage/homepage.component';
 import { featureHomeRoutes } from './lib.routes';
@@ -10,10 +11,11 @@ import { featureHomeRoutes } from './lib.routes';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule.forChild(featureHomeRoutes),
     BannerComponent,
     CardComponent,
     ExpandablePanelComponent,
-    RouterModule.forChild(featureHomeRoutes),
+    DetailsViewComponent,
   ],
   declarations: [HomepageComponent],
 })
--- pps/libs/ui/details-view/src/lib/details-view/details-view.component.ts
+++ pps/libs/ui/details-view/src/lib/details-view/details-view.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ButtonComponent } from '@pps/ui/button';
 
 @Component({
   selector: 'pps-details-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './details-view.component.html',
   styleUrl: './details-view.component.scss',
 })
-export class DetailsViewComponent {}
+export class DetailsViewComponent {
+  @Input() imageSrc: string = '';
+  @Input() buttonTitle: string = '';
+  @Input() paragraphContent: string = '';
+  @Input() mainTitle: string = '';
+  @Input() subTitle: string = '';
+}
