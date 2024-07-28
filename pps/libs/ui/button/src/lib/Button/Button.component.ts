9d12273a9c930cfdcb3d0eead9e1eb62a796c66e
Nikola Mircheski <nmircheski@gmail.com>
2024-07-28 14:07:41 +0200

    Fix commit

--- pps/libs/ui/button/src/lib/Button/Button.component.ts
+++ pps/libs/ui/button/src/lib/Button/Button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'pps-button',
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
   imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class ButtonComponent {
   /**
